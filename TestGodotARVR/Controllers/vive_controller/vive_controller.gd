b9e9f425992811d174dbad0d261b7430d0523515
BastiaanOlij <mux213@gmail.com>
2017-07-12 23:52:46 +1000

    Using new objects

--- TestGodotARVR/Controllers/vive_controller/vive_controller.gd
+++ TestGodotARVR/Controllers/vive_controller/vive_controller.gd
@@ -1,19 +1,11 @@
 extends Spatial
 
-var joy_id = -1
-
-func get_joy_id():
-	return joy_id
-	
-func set_joy_id(p_joy_id):
-	joy_id = p_joy_id
-
 func _ready():
 	set_process(true)
 
 func _process(delta):
-	# need to find out which joystick relates to this controller, maybe set through an export variable?
-	var rot = get_node("Trigger_origin").get_rotation_deg()
-#	rot.x = get_trigger() * -30.0
-	get_node("Trigger_origin").set_rotation_deg(rot)
-	
+	# Will implement this again shortly, we need to find out which joystick Id belongs to our controller
+#	var rot = get_node("Trigger_origin").get_rotation_deg()
+#	rot.x = Input.get_joy_axis(0, 0) * -30.0
+#	get_node("Trigger_origin").set_rotation_deg(rot)
+	pass
