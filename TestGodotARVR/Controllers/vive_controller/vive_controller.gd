1d943da883fc9f779a6599a107c828e14d29a62c
BastiaanOlij <mux213@gmail.com>
2017-07-15 13:39:44 +1000

    Fixed up a few small things around tracking

--- TestGodotARVR/Controllers/vive_controller/vive_controller.gd
+++ TestGodotARVR/Controllers/vive_controller/vive_controller.gd
@@ -1,11 +1,10 @@
-extends Spatial
+extends ARVRController
 
 func _ready():
 	set_process(true)
 
 func _process(delta):
-	# Will implement this again shortly, we need to find out which joystick Id belongs to our controller
-#	var rot = get_node("Trigger_origin").get_rotation_deg()
-#	rot.x = Input.get_joy_axis(0, 0) * -30.0
-#	get_node("Trigger_origin").set_rotation_deg(rot)
-	pass
+# Will implement this again shortly, we need to find out which joystick Id belongs to our controller
+	var rot = get_node("Trigger_origin").get_rotation_deg()
+	rot.x = get_joystick_axis(1) * -30.0
+	get_node("Trigger_origin").set_rotation_deg(rot)
