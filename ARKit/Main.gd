e33b3a6359fbf339db2d54acb151c78c237bf86f
BastiaanOlij <mux213@gmail.com>
2017-08-19 13:41:52 +1000

    Working on anchors

--- ARKit/Main.gd
+++ ARKit/Main.gd
@@ -1,5 +1,7 @@
 extends Spatial
 
+var arkit = null
+
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
@@ -7,6 +9,21 @@ func _ready():
 	if (arkit):
 		print("Found ARKit")
 		arkit.initialize()
+		
+		arkit.set_plane_detection_is_enabled(true)
+		get_node("toggle_plane_detection").set_text("Turn plane detection off")
+		
 		get_viewport().arvr = true
 	else:
 		print("Couldn't find ARKit")
+		get_node("toggle_plane_detection").set_text("No ARKIT")
+
+
+func _on_toggle_plane_detection():
+	if (arkit):
+		if (arkit.get_plane_detection_is_enabled()):
+			arkit.set_plane_detection_is_enabled(false)
+			get_node("toggle_plane_detection").set_text("Turn plane detection on")
+		else:
+			arkit.set_plane_detection_is_enabled(true)
+			get_node("toggle_plane_detection").set_text("Turn plane detection off")
---
+++ ARKit/anchor.gd
@@ -0,0 +1,15 @@
+extends ARVRAnchor
+
+# should move this into a subscene so we can add anchors as new ones are registered
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	set_process(true)
+	
+func _process(delta):
+	if get_is_active() == false:
+		visible = false
+	elif visible == false:
+		visible = true
+		get_node("../../GodotBall").translation = translation
