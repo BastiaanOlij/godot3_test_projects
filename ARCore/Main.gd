44beb348353438a1282640351cfd7103b8034cd1
Bastiaan Olij <mux213@gmail.com>
2019-03-09 23:36:31 +1100

    Finishing ARCore example

--- ARCore/Main.gd
+++ ARCore/Main.gd
@@ -5,48 +5,73 @@ extends Spatial
 # moves his/her phone around and we should have the spinning cube anchored in real space.
 # That gives us our starting point...
 
+export (PackedScene) var anchor_scene = null
+
 var angle = 0;
-var permissions = null
-var interface = null
+var arcore = null
+
+func tracker_added(tracker_name, tracker_type, tracker_id):
+	if tracker_type == ARVRServer.TRACKER_ANCHOR:
+		var new_anchor = anchor_scene.instance()
+		
+		new_anchor.tracker_id = tracker_id
+		new_anchor.set_name("anchor_" + str(tracker_id))
+		
+		$ARVROrigin.add_child(new_anchor)
+
+func tracker_removed(tracker_name, tracker_type, tracker_id):
+	if tracker_type == ARVRServer.TRACKER_ANCHOR:
+		var anchor = $ARVROrigin.get_node("anchor_" + str(tracker_id))
+		if anchor:
+			anchor.queue_free()
+
+func _add_log(text):
+	$Log.text = $Log.text + text + "\n"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# This code should be moved into our ARCore interface.
-	# It should handle the permissions
-	# Then we can do our normal ARVR initialise logic instead of doing it in _process
-	if Engine.has_singleton("AndroidPermissions"):
-		permissions = Engine.get_singleton("AndroidPermissions")
-		permissions.init(get_instance_id(), false)
-		
-		$Info.text = $Info.text + "Asking for camera permissions\n"
-		
-		permissions.requestCameraPermission()
+	# register our signals
+	ARVRServer.connect("tracker_added", self, "tracker_added")
+	ARVRServer.connect("tracker_removed", self, "tracker_removed")
+	
+	# ARCore initialisation should be in _ready
+	arcore = ARVRServer.find_interface('ARCore')
+	# arcore = ARVRServer.find_interface('Native mobile')
+	if arcore:
+		if arcore.initialize():
+			# this just means we started our initialisation process successfully
+			_add_log("Started ARCore")
+			get_viewport().arvr = true
+			
+			# assign our camera
+			get_viewport().get_camera().environment.background_camera_feed_id = arcore.get_camera_feed_id()
+		else:
+			_add_log("Failed to start ARCore")
 	else:
-		$Info.text = $Info.text + "Couldn't get android permissions\n"
-
-# Called when a request dialog is finished
-func _on_request_permission_result(request_code, permissions, granted):
-	if granted:
-		$Info.text = $Info.text + "Request code: " + str(request_code) + " granted\n"
+		_add_log("Failed to find ARCore")
+	
+	
 
 func _process(delta):
 	angle += delta
-	$Box.transform.basis = Basis(Vector3(1.0, 1.0, 1.0).normalized(), angle)
+	$Box.transform.basis = Basis(Vector3(1.0, 1.0, 1.0).normalized(), angle)	
 	
-	# Temporary workaround until we properly implement permissions.
-	# ARCore initialisation should be in _ready
-	if permissions and !interface:
-		if permissions.isCameraPermissionGranted():
-			interface = ARVRServer.find_interface('ARCore')
-			if interface:
-				if interface.initialize():
-					$Info.text = $Info.text + "Started ARCore\n"
-					get_viewport().arvr = true
-				else:
-					$Info.text = $Info.text + "Failed to start ARCore\n"
-			else:
-				$Info.text = $Info.text + "Failed to find ARCore\n"
+	var info_text = "FPS: " + str(Engine.get_frames_per_second()) + "\n"
 	
+	if arcore:
+		var status = arcore.get_tracking_status() 
+		if status == 4:
+			info_text += "Not tracking, move your device around for ARCore to detect features\n"
+		elif status == 0:
+			info_text += "Tracking a-ok\n"
+		elif status == 1:
+			info_text += "Insufficient tracking, moving your device to fast\n"
+		elif status == 2:
+			info_text += "Insufficient features, move further away or improve lighting conditions\n"
+		else:
+			info_text += "Unknown tracking status\n"
+			
+	$Info.text = info_text
 
 func _on_Button_pressed():
 	get_tree().quit()
