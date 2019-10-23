cb9513df0816235ea43d12380214326a229efe80
Rémi Verschelde <rverschelde@gmail.com>
2019-10-23 14:44:25 +0200

    ARCore: Update for current 3.2 PR

--- ARCore/Main.gd
+++ ARCore/Main.gd
@@ -5,29 +5,32 @@ extends Spatial
 # moves his/her phone around and we should have the spinning cube anchored in real space.
 # That gives us our starting point...
 
-var angle = 0;
-var permissions = null
+var angle = 0
 var interface = null
+var camera_permission = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# This code should be moved into our ARCore interface.
 	# It should handle the permissions
 	# Then we can do our normal ARVR initialise logic instead of doing it in _process
-	if Engine.has_singleton("AndroidPermissions"):
-		permissions = Engine.get_singleton("AndroidPermissions")
-		permissions.init(get_instance_id(), false)
-		
-		$Info.text = $Info.text + "Asking for camera permissions\n"
-		
-		permissions.requestCameraPermission()
+	camera_permission = Array(OS.get_granted_permissions()).has("android.permission.CAMERA")
+	if not camera_permission:
+		get_tree().connect("on_request_permissions_result", self, "_on_request_permission_result")
+		OS.request_permissions()
 	else:
-		$Info.text = $Info.text + "Couldn't get android permissions\n"
+		$Info.text += "Permission 'android.permission.CAMERA' already granted.\n"
+
 
 # Called when a request dialog is finished
-func _on_request_permission_result(request_code, permissions, granted):
+func _on_request_permission_result(permission, granted):
 	if granted:
-		$Info.text = $Info.text + "Request code: " + str(request_code) + " granted\n"
+		$Info.text += "Permission '" + permission + "' granted\n"
+		camera_permission = true
+	else:
+		$Info.text += "Permission '" + permission + "' refused\n"
+
 
 func _process(delta):
 	angle += delta
@@ -35,17 +38,16 @@ func _process(delta):
 	
 	# Temporary workaround until we properly implement permissions.
 	# ARCore initialisation should be in _ready
-	if permissions and !interface:
-		if permissions.isCameraPermissionGranted():
-			interface = ARVRServer.find_interface('ARCore')
-			if interface:
-				if interface.initialize():
-					$Info.text = $Info.text + "Started ARCore\n"
-					get_viewport().arvr = true
-				else:
-					$Info.text = $Info.text + "Failed to start ARCore\n"
+	if camera_permission and !interface:
+		interface = ARVRServer.find_interface('ARCore')
+		if interface:
+			if interface.initialize():
+				$Info.text = $Info.text + "Started ARCore\n"
+				get_viewport().arvr = true
 			else:
-				$Info.text = $Info.text + "Failed to find ARCore\n"
+				$Info.text = $Info.text + "Failed to start ARCore\n"
+		else:
+			$Info.text = $Info.text + "Failed to find ARCore\n"
 	
 
 func _on_Button_pressed():
